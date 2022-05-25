c6ceb6ad065930bf7b110cb044a31af78b035bc9
Romina Arce Aban <rarceaban@outlook.com>
2022-05-25 19:05:09 -0300

    port

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const Contenedor = require('./contenedor')
 
 const DBfile = 'products.json'
 const app = express()
+const PORT = process.env.PORT || 8080
 const contenedor = new Contenedor(DBfile)
 
 
@@ -20,6 +21,8 @@ app.get('/productsRandom', ( request, response) => {
  })
 
 
+ const server = app.listen(PORT, () => {
+    console.log(`Server http on ${PORT}...`)
+})
+server.on('error', error => console.log('Error on server', error))
 
-
-app.listen(8080)
